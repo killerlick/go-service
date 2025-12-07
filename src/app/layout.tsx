5b9235243b70e66e0cd587fc729e2f4b475d4011
malick <chimpanfeu@gmail.com>
2025-12-06 23:10:54 -0500

    ajout analytique de vercl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/next';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +35,7 @@ export default function RootLayout({
         <Header></Header>
         <main className="flex-grow">
                   {children}
+          <Analytics />
 
         </main>
         
